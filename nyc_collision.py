4f175104c1e4df6ec11552adc49cc9237ea56994
agarwal-ankit007 <47689721+agarwal-ankit007@users.noreply.github.com>
2020-06-07 22:14:15 +0530

    Update nyc_collision.py

--- nyc_collision.py
+++ nyc_collision.py
@@ -29,7 +29,7 @@ original_data = data
 st.header("where are the most people injured in NYC ?")
 injured_people = st.slider("No. of persons injured in vehicle collisions",0,19)
 
-st.map(data.query("injured_persons >= @injured_people")[['latitude','longitude']].dropna(how="any"))
+#st.map(data.query("injured_persons >= @injured_people")[['latitude','longitude']].dropna(how="any"))
 
 st.header("How many collisions occur during a given time of a day?")
 hour = st.slider("Hour to look at ",0,23)
@@ -58,9 +58,9 @@ st.write(pdk.Deck(
 
 
 st.subheader("Breakdown by minute between %i:00 and %i:00" %(hour,(hour+1) %24))
-filtered = data[
-	(data['date/time'].dt.hour >= hour) & (data['date/time'].dt.hour < (hour+1))
-]
+#filtered = data[
+#	(data['date/time'].dt.hour >= hour) & (data['date/time'].dt.hour < (hour+1))
+#]
 
 hist = np.histogram(filtered['date/time'].dt.minute , bins=60, range = (0,60))[0]
 
@@ -72,12 +72,12 @@ st.write(fig)
 st.header("Top 5 Dangerous streets by affected people")
 select = st.selectbox('Affected type of people',['pedestrians','cyclists','motorists'])
 
-if select == 'pedestrians':
-	st.write(original_data.query("injured_pedestrians >= 1")[["on street name","injured_pedestrians"]].sort_values(by = ['injured_pedestrians'],ascending = False).dropna(how='any')[:5])
-elif select == 'cyclists':
-	st.write(original_data.query("injured_cyclists >= 1")[["on street name","injured_cyclists"]].sort_values(by = ['injured_cyclists'],ascending = False).dropna(how='any')[:5])
-else:
-	st.write(original_data.query("injured_motorists >= 1")[["on street name","injured_motorists"]].sort_values(by = ['injured_motorists'],ascending = False).dropna(how='any')[:5])
+#if select == 'pedestrians':
+#	st.write(original_data.query("injured_pedestrians >= 1")[["on street name","injured_pedestrians"]].sort_values(by = ['injured_pedestrians'],ascending = False).dropna(how='any')[:5])
+#elif select == 'cyclists':
+#	st.write(original_data.query("injured_cyclists >= 1")[["on street name","injured_cyclists"]].sort_values(by = ['injured_cyclists'],ascending = False).dropna(how='any')[:5])
+#else:
+#	st.write(original_data.query("injured_motorists >= 1")[["on street name","injured_motorists"]].sort_values(by = ['injured_motorists'],ascending = False).dropna(how='any')[:5])
 
 if st.checkbox("show raw data",False):
 	st.subheader('Raw Data')
